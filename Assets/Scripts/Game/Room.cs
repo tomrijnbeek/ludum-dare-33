767d49c4a0c6566431e1be077c68c385fee6bf9a
Tom <tom@tomrijnbeek.nl>
2015-08-22 11:23:52 +0200

    Added basic scene with rooms.

---
+++ Assets/Scripts/Game/Room.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviourBase {
+
+	public Room[] connections = new Room[4];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Connect(Room room, int direction)
+	{
+		var invertDir = (direction + 2) % 4;
+		if (this.connections[direction] != null || room.connections[invertDir] != null)
+			throw new UnityException("Can't replace existing connections.");
+		this.connections[direction] = room;
+		room.connections[invertDir] = this;
+	}
+}
