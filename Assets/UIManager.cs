8f01d82c51e139533c932af08f493f6b7031e6d7
Tom <tom@tomrijnbeek.nl>
2015-08-23 19:06:31 +0200

    Added tutorial overlay.

---
+++ Assets/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : Singleton<UIManager> {
+
+	public Canvas tutorialCanvas;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DisableTutorialCanvas()
+	{
+		Destroy (tutorialCanvas.gameObject);
+		Time.timeScale = 1;
+	}
+}
