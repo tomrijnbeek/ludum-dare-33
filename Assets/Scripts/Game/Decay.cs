8c14e691c50ad2ac474e218cdd6b7329447aaeba
Tom <tom@tomrijnbeek.nl>
2015-08-23 22:20:40 +0200

    Added decay timers.

--- Assets/Scripts/Game/Decay.cs
+++ Assets/Scripts/Game/Decay.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class Decay : MonoBehaviourBase {
 
-	public float lifeTime;
+	public int progressBarHandle;
+
+	public float lifeTime, timeLeft;
 
 	// Use this for initialization
 	void Start () {
+		timeLeft = lifeTime;
+
+		progressBarHandle = ProgressCircleManager.Instance.MakeCircle(transform.position);
+		ProgressCircleManager.Instance.UpdateCircle(progressBarHandle, 1);
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		lifeTime -= Time.deltaTime;
+		timeLeft -= Time.deltaTime;
 
-		if (lifeTime <= 0)
+		ProgressCircleManager.Instance.UpdateCircle(progressBarHandle, timeLeft / lifeTime);
+
+		if (timeLeft <= 0)
+		{
+			ProgressCircleManager.Instance.DeleteCircle(progressBarHandle);
 			Destroy (this.gameObject);
+		}
 	}
 }
